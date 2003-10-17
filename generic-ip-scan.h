92a58707fba3d1feb8d30fec52dc5b3d9a991a33
Roy Hills <Roy.Hills@nta-monitor.com>
2003-10-17 11:33:43 +0000

    Removed port numbers for change from UDP to IP.
    git-svn-id: svn+ssh://svn.nta-monitor.com/trunk/c_code/tcp-scan@1000 062a1500-4a13-0410-a63b-ee65f32af78f

--- generic-ip-scan.h
+++ generic-ip-scan.h
@@ -17,12 +17,10 @@
 
 /* Defines */
 
-#define DEFAULT_DEST_PORT 12345		/* Default UDP destination port */
 #define DEFAULT_INTERVAL 10             /* Default delay between packets (ms) */
 #define DEFAULT_BACKOFF_FACTOR 1.5      /* Default timeout backoff factor */
 #define DEFAULT_RETRY 3                 /* Default number of retries */
 #define DEFAULT_TIMEOUT 500             /* Default per-host timeout in ms */
-#define DEFAULT_SOURCE_PORT 0           /* Default UDP source port */
 
 /* Functions */
 
