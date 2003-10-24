e055a884ad14f5e266d0e6099f385863d60f8f7e
Roy Hills <Roy.Hills@nta-monitor.com>
2003-10-24 19:51:29 +0000

    Initial revision
    git-svn-id: svn+ssh://svn.nta-monitor.com/trunk/c_code/tcp-scan@1014 062a1500-4a13-0410-a63b-ee65f32af78f

---
+++ tcp-scan.c
@@ -0,0 +1,401 @@
+/*
+ * The TCP Scanner (tcp-scan) is Copyright (C) 2003 Roy Hills,
+ * NTA Monitor Ltd.
+ *
+ * $Id$
+ *
+ * tcp-scan -- The TCP Scanner
+ *
+ * Author:	Roy Hills
+ * Date:	16 September 2003
+ *
+ * Usage:
+ *    tcp-scan [options] [host...]
+ *
+ * Description:
+ *
+ * tcp-scan sends the specified TCP packet to the specified hosts
+ * and displays any responses received.
+ * 
+ */
+
+#include "rawip-scan-engine.h"
+#include "tcp-scan.h"
+
+static char const rcsid[] = "$Id$";   /* RCS ID for ident(1) */
+
+/* Global variables */
+int ip_protocol = DEFAULT_IP_PROTOCOL;	/* IP Protocol */
+unsigned interval = DEFAULT_INTERVAL;	/* Interval between packets */
+unsigned retry = DEFAULT_RETRY;		/* Number of retries */
+unsigned timeout = DEFAULT_TIMEOUT;	/* Per-host timeout */
+float backoff_factor = DEFAULT_BACKOFF_FACTOR;	/* Backoff factor */
+char const scanner_name[] = "tcp-scan";
+char const scanner_version[] = "1.0";
+unsigned data_len;
+
+extern int verbose;	/* Verbose level */
+extern int debug;	/* Debug flag */
+extern char *local_data;		/* Local data from --data option */
+
+/*
+ *	display_packet -- Check and display received packet
+ *
+ *	Inputs:
+ *
+ *	n		The length of the received packet in bytes
+ *	packet_in	The received packet
+ *	he		The host entry corresponding to the received packet
+ *	recv_addr	IP address that the packet was received from
+ *
+ *      Returns:
+ *
+ *      None.
+ *
+ *      This should check the received packet and display details of what
+ *      was received in the format: <IP-Address><TAB><Details>.
+ */
+void
+display_packet(int n, char *packet_in, struct host_entry *he,
+               struct in_addr *recv_addr) {
+   char ip_str[MAXLINE];	/* IP address(es) to display at start */
+   char *cp;
+   int i;
+
+/*
+ *	Write the IP addresses to the output string.
+ */
+   cp = ip_str;
+   cp += sprintf(cp, "%s\t", inet_ntoa(he->addr));
+   if ((he->addr).s_addr != recv_addr->s_addr)
+      cp += sprintf(cp, "(%s) ", inet_ntoa(*recv_addr));
+   *cp = '\0';
+/*
+ *	We assume that any response is valid.
+ *	Display IP address, packet length and packet data (both as hex and
+ *	text).
+ */
+   cp = packet_in;
+   printf("%sResponse: len=%d, data=",
+             ip_str, n);
+   cp = packet_in;
+   for (i=0; i<n; i++) {
+      printf("%.2x", (unsigned char) *cp);
+      cp++;
+   }
+   printf(" (");
+   cp = packet_in;
+   for (i=0; i<n; i++) {
+      printf("%c", isprint(*cp)?*cp:'.');
+      cp++;
+   }
+   printf(")\n");
+}
+
+/*
+ *	send_packet -- Construct and send a packet to the specified host
+ *
+ *	Inputs:
+ *
+ *	s		IP socket file descriptor
+ *	he		Host entry to send to
+ *	ip_protocol	IP Protcol to use
+ *	last_packet_time	Time when last packet was sent
+ *
+ *      Returns:
+ *
+ *      None.
+ *
+ *      This must construct an appropriate packet and send it to the host
+ *      identified by "he" using the socket "s".
+ *      It must also update the "last_send_time" field for this host entry.
+ */
+void
+send_packet(int s, struct host_entry *he, int ip_protocol,
+            struct timeval *last_packet_time) {
+   struct sockaddr_in sa_peer;
+   char buf[MAXIP];
+   int buflen;
+   NET_SIZE_T sa_peer_len;
+   static int first_time_through=1;
+   static int tcp_port;
+   static uint32_t source_address;
+   struct iphdr *iph = (struct iphdr *) buf;
+   struct tcphdr *tcph = (struct tcphdr *) (buf + sizeof(struct iphdr));
+   struct pseudo_hdr {	/* For computing TCP checksum */
+      uint32_t s_addr;
+      uint32_t d_addr;
+      uint8_t  mbz;
+      uint8_t  proto;
+      uint16_t len;
+   };
+   /* Position pseudo header just before the TCP header */
+   struct pseudo_hdr *pseudo = (struct pseudo_hdr *) (buf + sizeof(struct ip) -
+   sizeof(struct pseudo_hdr));
+/*
+ *	Initialise static packet data.
+ *	We can't do this in initialise() because local_data is not available
+ *	in that function.
+ */
+   if (first_time_through) {
+      tcp_port = atoi(local_data);
+      source_address = get_source_ip("eth0");
+      first_time_through=0;
+   }
+/*
+ *	Check that the host is live.  Complain if not.
+ */
+   if (!he->live) {
+      warn_msg("***\tsend_packet called on non-live host entry: SHOULDN'T HAPPEN");
+      return;
+   }
+/*
+ *	Set up the sockaddr_in structure for the host.
+ */
+   memset(&sa_peer, '\0', sizeof(sa_peer));
+   sa_peer.sin_family = AF_INET;
+   sa_peer.sin_addr.s_addr = he->addr.s_addr;
+   sa_peer_len = sizeof(sa_peer);
+/*
+ *	Construct the pseudo header (for TCP checksum purposes).
+ *	Note that this overlaps the IP header and gets overwritten later.
+ */
+   memset(pseudo, '\0', sizeof(struct pseudo_hdr));
+   pseudo->s_addr = source_address;
+   pseudo->d_addr = he->addr.s_addr;
+   pseudo->proto  = ip_protocol;
+   pseudo->len    = htons(sizeof(struct tcphdr));
+/*
+ *	Construct the TCP header.
+ */
+   memset(tcph, '\0', sizeof(struct tcphdr));
+   tcph->source = htons(5001);
+   tcph->dest = htons(tcp_port);
+   tcph->seq = htonl(0xdeadbeef);
+   tcph->doff = 5;	/* 5 * 32bit longwords */
+   tcph->syn = 1;
+   tcph->window = htons(5840);
+   tcph->check = in_cksum((uint16_t *)pseudo, sizeof(struct pseudo_hdr) +
+                 sizeof(struct tcphdr));
+/*
+ *	Construct the IP Header.
+ *	This overwrites the now unneeded pseudo header.
+ */
+   memset(iph, '\0', sizeof(struct iphdr));
+   iph->ihl = 5;	/* 5 * 32-bit longwords = 20 bytes */
+   iph->version = 4;
+   iph->tot_len = sizeof(struct iphdr) + sizeof(struct tcphdr);
+   iph->id = 0;		/* Linux kernel fills this in */
+   iph->ttl = 64;
+   iph->protocol = ip_protocol;
+   iph->check = 0;	/* Linux kernel fills this in */
+   iph->saddr = source_address;
+   iph->daddr = he->addr.s_addr;
+/*
+ *	Copy the required data into the output buffer "buf" and set "buflen"
+ *	to the number of bytes in this buffer.
+ */
+   buflen=sizeof(struct iphdr) + sizeof(struct tcphdr);
+/*
+ *	Update the last send times for this host.
+ */
+   if ((gettimeofday(last_packet_time, NULL)) != 0) {
+      err_sys("gettimeofday");
+   }
+   he->last_send_time.tv_sec  = last_packet_time->tv_sec;
+   he->last_send_time.tv_usec = last_packet_time->tv_usec;
+   he->num_sent++;
+/*
+ *	Send the packet.
+ */
+   if (debug) {print_times(); printf("send_packet: #%u to host entry %u (%s) tmo %d\n", he->num_sent, he->n, inet_ntoa(he->addr), he->timeout);}
+   if (verbose > 1)
+      warn_msg("---\tSending packet #%u to host entry %u (%s) tmo %d", he->num_sent, he->n, inet_ntoa(he->addr), he->timeout);
+   if ((sendto(s, buf, buflen, 0, (struct sockaddr *) &sa_peer, sa_peer_len)) < 0) {
+      err_sys("sendto");
+   }
+}
+
+/*
+ *      initialise -- Protocol-specific initialisation routine.
+ *
+ *      Inputs:
+ *
+ *      None.
+ *
+ *      Returns:
+ *
+ *      None.
+ *
+ *      This is called once before any packets have been sent.  It can be
+ *      used to perform any required initialisation.  It does not have to
+ *      do anything.
+ */
+void
+initialise(void) {
+}
+
+/*
+ *      clean_up -- Protocol-specific Clean-Up routine.
+ *
+ *      Inputs:
+ *
+ *      None.
+ *
+ *      Returns:
+ *
+ *      None.
+ *
+ *      This is called once after all hosts have been processed.  It can be
+ *      used to perform any tidying-up or statistics-displaying required.
+ *      It does not have to do anything.
+ */
+void
+clean_up(void) {
+}
+
+/*
+ *      local_version -- Scanner-specific version function.
+ *
+ *      Inputs:
+ *
+ *      None.
+ *
+ *      Returns:
+ *
+ *      None.
+ *
+ *      This should output the scanner-specific version number to stderr.
+ */
+void
+local_version(void) {
+/* We use rcsid here to prevent it being optimised away */
+   fprintf(stderr, "%s\n", rcsid);
+}
+
+/*
+ *      local_help -- Scanner-specific help function.
+ *
+ *      Inputs:
+ *
+ *      None.
+ *
+ *      Returns:
+ *
+ *      None.
+ *
+ *      This should output the scanner-specific usage for the --data option
+ *      (if any).
+ */
+void
+local_help(void) {
+   fprintf(stderr, "\n--data=<d> or -D <d>\tSpecify packet contents in hex.\n");
+   fprintf(stderr, "\t\t\tE.g. --data=deadbeef would specify the 4-byte IP\n");
+   fprintf(stderr, "\t\t\tpayload: 0xde, 0xad, 0xbe, 0xef.\n");
+}
+
+/*
+ *      local_add_host -- Protocol-specific add host routine.
+ *
+ *      Inputs:
+ *
+ *      name = The Name or IP address of the host.
+ *      timeout = The initial host timeout in ms.
+ *
+ *      Returns:
+ *
+ *      0 (Zero) if this function doesn't need to do anything, or
+ *      1 (One) if this function replaces the generic add_host function.
+ *
+ *      This routine is called once for each specified host.
+ *
+ *      This protocol-specific add host routine can replace the generic
+ *      rawip-scan add-host routine if required.  If it is to replace the
+ *      generic routine, then it must perform all of the add_host functions
+ *      and return 1.  Otherwise, it must do nothing and return 0.
+ */
+int
+local_add_host(char *name, unsigned timeout) {
+   return 0;
+}
+
+/*
+ *      Convert a two-digit hex string with to unsigned int.
+ *      E.g. "0A" would return 10.
+ *      Note that this function does no sanity checking, it's up to the
+ *      caller to ensure that *cptr points to at least two hex digits.
+ *      This function is a modified version of hstr_i at www.snippets.org.
+ */
+unsigned int hstr_i(char *cptr)
+{
+      unsigned int i;
+      unsigned int j = 0;
+      int k;
+
+      for (k=0; k<2; k++) {
+            i = *cptr++ - '0';
+            if (9 < i)
+                  i -= 7;
+            j <<= 4;
+            j |= (i & 0x0f);
+      }
+      return(j);
+}
+
+/* Standard BSD internet checksum routine */
+uint16_t in_cksum(uint16_t *ptr,int nbytes) {
+
+   register uint32_t sum;
+   uint16_t oddbyte;
+   register uint16_t answer;
+
+/*
+ * Our algorithm is simple, using a 32-bit accumulator (sum),
+ * we add sequential 16-bit words to it, and at the end, fold back
+ * all the carry bits from the top 16 bits into the lower 16 bits.
+ */
+
+   sum = 0;
+   while (nbytes > 1)  {
+      sum += *ptr++;
+      nbytes -= 2;
+   }
+
+/* mop up an odd byte, if necessary */
+   if (nbytes == 1) {
+      oddbyte = 0;            /* make sure top half is zero */
+      *((u_char *) &oddbyte) = *(u_char *)ptr;   /* one byte only */
+      sum += oddbyte;
+   }
+
+/*
+ * Add back carry outs from top 16 bits to low 16 bits.
+ */
+
+   sum  = (sum >> 16) + (sum & 0xffff);    /* add high-16 to low-16 */
+   sum += (sum >> 16);                     /* add carry */
+   answer = ~sum;          /* ones-complement, then truncate to 16 bits */
+   return(answer);
+}
+
+uint32_t get_source_ip(char *devname) {
+   int sockfd;
+   struct ifreq ifconfig;
+   struct sockaddr_in sa;
+
+   strcpy(ifconfig.ifr_name, devname);
+
+/* Create UDP socket */
+   if ((sockfd=socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
+      perror("socket");
+      exit(1);
+   }
+/* Obtain IP address for specified interface */
+   if ((ioctl(sockfd, SIOCGIFADDR, &ifconfig)) != 0) {
+      perror("ioctl");
+      exit(1);
+   }
+
+   memcpy(&sa, &ifconfig.ifr_ifru.ifru_addr, sizeof(sa));
+   return sa.sin_addr.s_addr;
+}
---
+++ tcp-scan.h
@@ -0,0 +1,34 @@
+/*
+ * The RAWIP Scan Engine (rawip-scan-engine) is Copyright (C) 2003 Roy Hills,
+ * NTA Monitor Ltd.
+ *
+ * $Id$
+ *
+ * tcp-scan.h -- Header file for TCP protocol specific scanner
+ *
+ * Author:	Roy Hills
+ * Date:	16 September 2003
+ *
+ * This header file contains definitions required by only the protocol-
+ * specific code.
+ */
+
+/* Includes */
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+
+/* Defines */
+
+#define DEFAULT_IP_PROTOCOL 6		/* Default IP Protocol */
+#define DEFAULT_INTERVAL 10             /* Default delay between packets (ms) */
+#define DEFAULT_BACKOFF_FACTOR 1.5      /* Default timeout backoff factor */
+#define DEFAULT_RETRY 3                 /* Default number of retries */
+#define DEFAULT_TIMEOUT 500             /* Default per-host timeout in ms */
+
+/* Functions */
+
+unsigned int hstr_i(char *);
+uint16_t in_cksum(uint16_t *, int);
+uint32_t get_source_ip(char *);
