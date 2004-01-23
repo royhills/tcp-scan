435ce75b3fe5ffaad8a8562d2bcb7a203a194dcc
Roy Hills <Roy.Hills@nta-monitor.com>
2004-01-23 12:55:07 +0000

    Added make_message() function.
    git-svn-id: svn+ssh://svn.nta-monitor.com/trunk/c_code/tcp-scan@1285 062a1500-4a13-0410-a63b-ee65f32af78f

--- rawip-scan-engine.c
+++ rawip-scan-engine.c
@@ -730,6 +730,46 @@ print_times(void) {
 }
 
 /*
+ * make_message -- allocate a sufficiently large string and print into it.
+ *
+ * Inputs:
+ *
+ * Format and variable number of arguments.
+ *
+ * Outputs:
+ *
+ * Pointer to the string,
+ *
+ * The code for this function is from the Debian Linux "woody" sprintf man
+ * page.  Modified slightly to use wrapper functions for malloc and realloc.
+ */
+char *
+make_message(const char *fmt, ...) {
+   int n;
+   /* Guess we need no more than 100 bytes. */
+   size_t size = 100;
+   char *p;
+   va_list ap;
+   p = Malloc (size);
+   while (1) {
+      /* Try to print in the allocated space. */
+      va_start(ap, fmt);
+      n = vsnprintf (p, size, fmt, ap);
+      va_end(ap);
+      /* If that worked, return the string. */
+      if (n > -1 && n < size)
+         return p;
+      /* Else try again with more space. */
+      if (n > -1)    /* glibc 2.1 */
+         size = n+1; /* precisely what is needed */
+      else           /* glibc 2.0 */
+         size *= 2;  /* twice the old size */
+      p = Realloc (p, size);
+   }
+}
+
+
+/*
  *	rawip_scan_version -- display version information
  *
  *	Inputs:
